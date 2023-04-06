0c43508fc7cb092f853441632749e3538821c981
Micaela Analia Jofre <mkjofre@gmail.com>
2023-04-06 17:27:18 -0300

    menu bio

--- contacto/quform/js/scripts.js
+++ contacto/quform/js/scripts.js
@@ -65,3 +65,19 @@ jQuery(document).ready(function($) {
 		$.preloadImages(images);
 	});
 })(jQuery);
+ //  menu    ------------------
+ var nb = $(".nav-button"),
+ nh = $("nav"),
+ an = $("nav, .nav-button ");
+function showMenu() {
+ nb.removeClass("vis-m");
+ nh.slideDown(500);
+}
+function hideMenu() {
+ nb.addClass("vis-m");
+ nh.slideUp(500);
+}
+nb.on("click", function () {
+ if ($(this).hasClass("vis-m")) showMenu();
+ else hideMenu();
+});
